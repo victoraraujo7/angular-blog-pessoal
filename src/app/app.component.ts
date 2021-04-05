7817854ffc7d5652819e5e2313b94e81baf6f2f0
Victor <victor.araujo07@hotmail.com>
2021-04-05 11:40:58 -0300

    criado metodos post e get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { AuthService } from './service/auth.service';
 
 @Component({
@@ -6,9 +8,17 @@ import { AuthService } from './service/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
   constructor(
+    private router: Router,
     public auth: AuthService
   ){}
+
+  ngOnInit(){
+
+    if(environment.token == ''){
+     this.router.navigate(['/entrar'])
+    }
+  }
 }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +15,21 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  user: User = new User()
+  idUser: number = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(){
@@ -18,6 +37,52 @@ export class InicioComponent implements OnInit {
     if(environment.token == ''){
      this.router.navigate(['/entrar'])
     }
+    this.getAllTemas()
+    this.findAllPostagem()
   }
 
+  findUserById(){
+    this.authService.getByIdUser(this.idUser).subscribe((resp) => {
+      this.user = resp;
+    })
+  }
+
+  findAllPostagem(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp;
+    })
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp;
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp;
+    })
+  }
+
+  publicar(){
+    this.postagem.tema = new Tema()
+    this.postagem.tema.id = this.idTema
+    this.postagem.usuario = new User()
+    this.postagem.usuario.id = this.idUser
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp;
+      alert('Postagem realizada com sucesso!');
+      this.postagem = new Postagem();
+      this.findAllPostagem();
+    })
+  }
+
+  printar(){
+    console.log(this.postagem.texto)
+    console.log(environment.token)
+  }
+  
+  
 }
